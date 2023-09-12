1f75789d4ca19ed5729d976c6c75760dfbba43de
RahafZone <132655136+RahafZone@users.noreply.github.com>
2023-09-12 22:58:11 +0300

    Add files via upload

---
+++ cytron.ino
@@ -0,0 +1,32 @@
+const int softStartDuration = 2000; 
+const float alpha = 0.2; 
+
+const int motorPin = 9;
+
+unsigned long startTime; 
+float filteredValue = 0.0; 
+int desiredValue = 0; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    desiredValue = Serial.parseInt();
+    Serial.print("Desired Speed: ");
+    Serial.println(desiredValue);
+    startTime = millis(); 
+  }
+  unsigned long elapsedTime = millis() - startTime;
+
+  float filterFactor = min(1.0, (float)elapsedTime/softStartDuration);
+  float softStartValue = filterFactor * desiredValue;
+
+  filteredValue = (alpha * softStartValue)+((1 - alpha) * filteredValue);
+
+  int motorSpeed = map(filteredValue, 0, 1023, 0, 255);
+
+  analogWrite(motorPin, motorSpeed);
+}
---
+++ cytron_2.ino
@@ -0,0 +1,22 @@
+const float alpha = 0.2; 
+
+const int motorPin = 9; 
+
+float filteredValue = 0.0; 
+int speedValue = 0; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    speedValue = Serial.parseInt();
+    Serial.print("Desired Speed: ");
+    Serial.println(speedValue);
+  }
+  filteredValue = (alpha * speedValue) + ((1 - alpha) * filteredValue);
+  int motorSpeed = map(filteredValue, 0, 1023, 0, 255);
+  analogWrite(motorPin, motorSpeed);
+}
