dd7ca4e3f1fa37a21545b4c0de01b49d3c5b8f09
mostafa0ali11 <144283725+mostafa0ali11@users.noreply.github.com>
2023-09-15 00:17:19 +0300

    Update TASK_7.2_PID_control.ino

--- Task_7.2/PID_control_on_arduino.ino
+++ Task_7.2/PID_control_on_arduino.ino
@@ -1,57 +1,44 @@
 /*
-sketch belongs to this video: https://youtu.be/crw0Hcc67RY
-write by Moz for YouTube changel logMaker360
-4-12-2017
+Team:11
+Task:7.2
 */
 
 #include <PID_v1.h>
 
-
-double Setpoint = 90.0;  // Desired flow rate in CFM
-double Input;            // Flow rate from the flow meter
-double Output;           // Suction mechanism control
-double Kp = 0.5;         // Proportional gain
-double Ki = 0.2;         // Integral gain
-double Kd = 0.1;         // Derivative gain
+double Setpoint ; // will be the desired value
+double Input; // Water Flow sensor
+double Output ; //Water pump
+//PID parameters
+double Kp=0, Ki=10, Kd=0; 
  
-// Create PID instance
+//create PID instance 
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
  
-const int FLOW_METER_PIN = 2;
-  
-// Pin for the flow meter
-
 void setup()
 {
-  Serial.begin(9600);
-
-  // Initialize the flow meter
-  pinMode(FLOW_METER_PIN, INPUT);
-
-  // Turn on the PID
+  
+  Serial.begin(9600);   
+  //Hardcode the brigdness value
+  Setpoint = 90; /*90 CFM*/
+  //Turn the PID on
   myPID.SetMode(AUTOMATIC);
-
   //Adjust PID values
   myPID.SetTunings(Kp, Ki, Kd);
-
 }
-
+ 
 void loop()
 {
-  //Read the value from the light sensor. Analog input : 0 to 1024. We map is to a value from 0 to 255 as it's used for our PWM function.
-  Input = map(analogRead(5), 0, 1024, 0, 255);  // photo senor is set on analog pin 5
+  //Read the value from the Water Flow sensor. Analog input : 0 to 1024. We map is to a value from 0 to 255 as it's used for our PWM function.
+  Input = map(analogRead(5), 0, 1024, 0, 255);  // Water Flow sensor is set on analog pin 5
   //PID calculation
   myPID.Compute();
   //Write the output as calculated by the PID function
-  analogWrite(3,Output); //LED is set to digital 3 this is a pwm pin. 
+  analogWrite(3,Output); //Water pump driver is set to digital 3 this is a pwm pin. 
   //Send data by serial for plotting 
-  Serial.print("Input: ");
   Serial.print(Input);
-  Serial.print(" CFM, Output: ");
-  Serial.print(Output);
-  Serial.println();
+  Serial.print(" ");
+  Serial.println(Output);
+  Serial.print(" ");  
   Serial.println(Setpoint);
-
-  // Delay for a suitable time before the next iteration
-  delay(100);
-} 
+//  delay(100); 
+}
